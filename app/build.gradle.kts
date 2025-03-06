66260aa3166df5ebb338109a3f93e7b17bcbce72
Houvven <houvven@gmail.com>
2025-03-06 15:19:46 +0800

    Update app version to 1.0.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         minSdk = 33
         targetSdk = 35
         versionCode = 1
-        versionName = "1.0"
+        versionName = "1.0.0"
     }
     buildTypes {
         release {
